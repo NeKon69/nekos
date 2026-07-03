d83471c67d6ad6d7e777e933cada5aa6c9de6c30
NeKon69 <nobodqwe@gmail.com>
2026-07-03 18:20:59 +0300

    changes

--- kernel/src/nekos/kernel.cpp
+++ kernel/src/nekos/kernel.cpp
@@ -3,13 +3,18 @@
 #error "This needs to be compiled with a ix86-elf compiler"
 #endif
 
+#if NEKOS_KERNEL_TESTS
 #include "nekos/drivers/serial.hpp"
 #include "test.hpp"
+#endif
 
 extern "C" void kernel_main() {
-  kprintf("Hello how is your day?");
+#if NEKOS_KERNEL_TESTS
   drivers::Serial Serial;
 
   tests::init(Serial);
   tests::runAll();
+#else
+  kprintf("Hello how is your day?");
+#endif
 }
